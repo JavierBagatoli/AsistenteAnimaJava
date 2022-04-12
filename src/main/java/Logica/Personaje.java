4856380157fae89c80f0665321832ffe8cee486f
JavierBagatoli <javi.bagatoli@gmail.com>
2022-04-12 10:03:28 -0300

    primera muestra

---
+++ src/main/java/Logica/Personaje.java
@@ -0,0 +1,14 @@
+package Logica;
+
+/**
+ * @author JavW11trial
+ */
+public class Personaje {
+    private String nombre;
+    private int vida;
+    private int ha;
+    private int hd;
+    private int[] d = new int[8];
+    
+    
+}
