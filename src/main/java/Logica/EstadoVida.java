489e6e54d43bf4515ec1deac39e293e57acfe293
JavierBagatoli <javi.bagatoli@gmail.com>
2022-04-12 11:18:57 -0300

    Clase parametrizada EstadoVida

---
+++ src/main/java/Logica/EstadoVida.java
@@ -0,0 +1,50 @@
+package Logica;
+
+/**
+ *
+ * @author JavW11trial
+ * @param <A>
+ * @param <B>
+ */
+public class EstadoVida<A extends Integer, B extends Integer> {
+    A vidaMaxima;
+    B vidaActual;
+
+    public EstadoVida(A vidaMaxima, B vidaActual) {
+        this.vidaMaxima = vidaMaxima;
+        this.vidaActual = vidaActual; 
+    }
+
+    public EstadoVida(A vidaMaxima) {
+        this.vidaMaxima = vidaMaxima;
+        this.vidaActual = (B) this.vidaMaxima;
+    }
+    
+    private void aumentarVida(Integer aumentoDeVida){
+        int NuevaVida = vidaMaxima + aumentoDeVida;
+        vidaMaxima = (A) Integer.valueOf(NuevaVida);
+    }
+    
+    public void comprobarYcurar(B curarVida){
+        if(faltaVida()){
+            if(curarTodaVida(curarVida)){
+                vidaActual = (B) vidaMaxima;
+            }else{
+                int vidaAlCurar = vidaActual + curarVida;
+                vidaActual = (B) Integer.valueOf(vidaAlCurar); 
+            }
+        }
+    }
+    private boolean faltaVida(){
+        return vidaMaxima > vidaActual;
+    }
+    
+    private boolean curarTodaVida(B curarVida){
+        return (curarVida > vidaMaxima - vidaActual);
+    }
+    
+    public void daniarVida(B danio){
+        int vidaDaniada = vidaActual - danio;
+        vidaActual = (B) Integer.valueOf(vidaDaniada);
+    }
+}
--- src/main/java/Logica/Personaje.java
+++ src/main/java/Logica/Personaje.java
@@ -4,11 +4,53 @@ package Logica;
  * @author JavW11trial
  */
 public class Personaje {
-    private String nombre;
-    private int vida;
-    private int ha;
-    private int hd;
-    private int[] d = new int[8];
+    private EstadoVida<Integer,Integer> vida;
+    private int habilidadAtaque;
+    private int habilidadDefensa;
+    private int[] vectorDeDefensas = new int[8];
+
+    public EstadoVida<Integer, Integer> getVida() {
+        return vida;
+    }
+
+    public int getHabilidadAtaque() {
+        return habilidadAtaque;
+    }
+
+    public void setHabilidadAtaque(int habilidadAtaque) {
+        this.habilidadAtaque = habilidadAtaque;
+    }
+
+    public int getHabilidadDefensa() {
+        return habilidadDefensa;
+    }
+
+    public void setHabilidadDefensa(int habilidadDefensa) {
+        this.habilidadDefensa = habilidadDefensa;
+    }
+
+    public int[] getVectorDeDefensas() {
+        return vectorDeDefensas;
+    }
+
+    public void setVectorDeDefensas(int[] vectorDeDefensas) {
+        this.vectorDeDefensas = vectorDeDefensas;
+    }
+
+    
+    
+    private void curar(int cantidadVida){
+        this.vida.comprobarYcurar(cantidadVida);
+    }
+    
+    private void daniar(int cantidadVida){
+        this.vida.daniarVida(cantidadVida);
+    }
+    
+    public Personaje(int vidaMaxima) {
+        this.vida = new EstadoVida<Integer,Integer>(vidaMaxima,vidaMaxima);
+    }
+    
     
     
 }
